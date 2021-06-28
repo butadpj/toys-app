ed5ccafe32314fe53a822b27888bee6003b2485f
Paul John Butad <butadpj@gmail.com>
2021-06-28 12:22:22 +0800

    Add first_name & last_name column to Users

--- app/models/my_toy.rb
+++ app/models/my_toy.rb
@@ -1,5 +1,9 @@
 class MyToy < ApplicationRecord
     belongs_to :user
-    
+
+    delegate :username, to: :user
+    delegate :first_name, to: :user
+    delegate :last_name, to: :user
+
     has_many_attached :image    
 end
